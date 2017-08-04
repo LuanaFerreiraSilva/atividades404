961d1283de91d623033b30e1bef3923464753434
LuanaFerreiraSilva <macilu302@gmail.com>
2017-08-04 10:28:08 -0300

    Add files via upload

---
+++ atividade listas_q9.py
@@ -0,0 +1,20 @@
+#9.Faça um programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu
+#respectivo vetor. Imprima a idade e a altura na ordem inversa a ordem lida.
+def  Idade_Altura():
+    idade = []
+    altura = []
+    i=1
+    while i <=5:
+        ida = int(input("Digite a idade: "))
+        alt = float(input("Digite a altura: "))
+        print("\n")
+        i+=1
+        idade.append(ida)
+        altura.append(alt)
+
+    idade.reverse()
+    print(idade)
+    altura.reverse()
+    print(altura)
+        
+Idade_Altura()

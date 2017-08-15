578357cf5fcea264c2660ad167b5f5afd34237a9
LuanaFerreiraSilva <macilu302@gmail.com>
2017-08-15 15:18:15 -0300

    Add files via upload

---
+++ atividade repeticoes_q8.py
@@ -0,0 +1,10 @@
+#08.Criar um algoritmo que leia um número que será o limite superior de um intervalo e o incremento
+#(inc). Imprimir todos os números naturais no intervalo de zero até o limite superior. Suponha que
+#o incremento é maior do que zero e o limite superior maior que o incremento.
+
+limite = int(input("Limite superior do intervalo: "))
+inc = int(input("Incremento: "))
+
+for i in range(0, limite ,inc):
+
+	print(i, end = "  ")

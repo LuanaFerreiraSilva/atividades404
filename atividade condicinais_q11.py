1d5a30a844dc34479305cbaac26765f2c5cd3f0e
LuanaFerreiraSilva <macilu302@gmail.com>
2017-08-18 11:17:23 -0300

    Update atividade condicinais_q11.py

--- atividade condicinais_q11.py
+++ atividade condicinais_q11.py
@@ -1,8 +1,8 @@
-#11. Crie	 uma	 função	 que	 recebe	 a	 temperatura	 de	 uma	 pessoa	 e	 retorna	 “Está	 com	 febre”	 ou	 “Sem	 febre”.	
-#Considere	o	valor	base	como	36.5.
-def termometro(t):
+#11. Crie uma funçã que recebe a  temperatura de uma pessoa e retorna "Está com febre" ou "Sem febre".	
+#Considere o valor base	como 36.5.
+def termometro(temperatura):
 
-	if t > 36.5:
+	if temperatura > 36.5:
 
 		return "Está com febre"
 
@@ -12,10 +12,11 @@ def termometro(t):
 
 try:
 
-	t = float(input("Forneça um valor real correspondente à temperatura corporal de uma pessoa: "))
+	temperatura = float(input("Forneça um valor real correspondente à temperatura corporal de uma pessoa: "))
 
-	print(termometro(t))
+	print(termometro(temperatura))
 
 except:
 
 	print("Não foi fornecido um valor real.")
+

b9c64f4de109037d1f538e95106bb9ae409130cd
LuanaFerreiraSilva <macilu302@gmail.com>
2017-08-22 11:42:49 -0300

    Update atividade condicionais_q17.py

--- atividade condicionais_q17.py
+++ atividade condicionais_q17.py
@@ -1,31 +1,30 @@
-#17. A	partir	de	dois	números	fornecidos	pelo	usuário,	escreva	uma	das	seguintes	mensagens:
-#Os	dois	são	pares
-#Os	dois	são	impares
-#O primeiro	é	par	e	o	segundo	é	ímpar
-#O	primeiro	é	ímpar	e	o	segundo	é	par
-def dezessete(x, y):
+#17. A	partir	de dois	números	fornecidos pelo	usuário, escreva uma das seguintes mensagens: Os dois são pares
+#Os dois são impares.
+#O primeiro é par e o segundo é	ímpar.
+#O primeiro é ímpar e o segundo	é par.
+def dezessete(a, b):
 
-	if x % 2 == 0 and y % 2 == 0:
+	if a % 2 == 0 and b % 2 == 0:
 
 		return "Os dois são pares"
 
-	elif x % 2 != 0 and y % 2 != 0:
+	elif a % 2 != 0 and b % 2 != 0:
 
 		return "Os dois são ímpares"
 
-	elif x % 2 == 0 and y % 2 != 0:
+	elif a % 2 == 0 and b % 2 != 0:
 
 		return "O primeiro é par e o segundo é ímpar"
 
-	elif x % 2 != 0 and y % 2 == 0:
+	elif a % 2 != 0 and b % 2 == 0:
 
 		return "O primeiro é ímpar e o segundo é par"
 
 try:
 
-	x, y = int(input("Forneça dois números inteiros: ")), int(input())
+	a, b = int(input("Forneça dois números inteiros: ")), int(input())
 
-	print(dezessete(x, y))
+	print(dezessete(a, b))
 
 except:
 

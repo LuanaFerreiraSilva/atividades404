8209fdbdc35339391dc87207d89a3b5d0181ce47
LuanaFerreiraSilva <macilu302@gmail.com>
2017-08-30 16:12:46 +0200

    Create atividade dicionarios_q1.py

---
+++ atividade dicionarios_q1.py
@@ -0,0 +1,10 @@
+#1.Crie um dicionário e armazene nele os seus dados: nome, idade, telefone, endereço. Imprima todos os
+dados usando o padrão chave: valor.
+
+Dados = {"Nome" : "Luana Silva",
+             "Idade" : 18,
+             "Telefone" : "(86) 3212-5678",
+             "Endereço" : "Quadra 1, Casa 12, Rua 123" }
+ 
+for i in Dados:
+    	print(i + ": %s" % Dados[i])
